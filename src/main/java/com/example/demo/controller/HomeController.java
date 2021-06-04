dbed2536b4a0343851f776a9a02b36532397597c
ChanhDai2702 <daiviet1023@gmail.com>
2021-06-05 00:42:45 +0700

    first comm sit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,16 @@
+//package com.example.demo.controller;
+//
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class HomeController {
+//	
+//	@RequestMapping(value = "/abc")
+//	public String a() {
+//		
+//		return "abc";
+//		
+//	}
+//}
